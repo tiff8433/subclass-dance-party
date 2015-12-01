0eb691faa21e32d87fa1490863f0028769ddda6e
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-01 08:19:28 -0800

    Implements blinkyDancer $node and step function

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,60 +3,16 @@
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps){
   console.log(this);
   MakeDancer.call(this, top, left, timeBetweenSteps);
- // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-  // var oldStep = blinkyDancer.step;
-
 };
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
 MakeBlinkyDancer.prototype.constructor = MakeBlinkyDancer;
 
-MakeBlinkyDancer.prototype.oldStep = MakeBlinkyDancer.prototype.step;
 MakeBlinkyDancer.prototype.step = function(){
-  MakeBlinkyDancer.prototype.oldStep();
+  MakeDancer.prototype.step.call(this);
   this.$node.toggle();
 };
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-//   makeDancer.call(this);
-//   this.$node = $('<span class="blinkyDancer"></span>');
-//   // this.oldStep = this.step;
-// };
-
-// makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-// makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-
-// makeBlinkyDancer.prototype.step = function(){
-//   makeDancer.prototype.step.call(this);
-//   this.$node.toggle();
-// };
--- src/dancer.js
+++ src/dancer.js
@@ -7,38 +7,17 @@ var MakeDancer = function(top, left, timeBetweenSteps){
   console.log(this);
   this.step();
   this.setPosition(top, left);
-}
+};
 
 MakeDancer.prototype.step = function(){
   var context = this;
   setTimeout(function(){context.step.call(context)}, context.timeBetweenSteps);
-}
+};
 MakeDancer.prototype.setPosition = function(top, left){
-      var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
-}
-
-// var makeDancer = function(top, left, timeBetweenSteps){
-//   this.top = top;
-//   this.left = left;
-//   this.timeBetweenSteps = timeBetweenSteps;
-//   this.$node = $('<span class="dancer"></span>');
-//   // this.step();
-//   // this.setPosition(top, left);
-// }
-
-// makeDancer.prototype.step = function(){
-//   setTimeout(this.step.bind(this), timeBetweenSteps);
-// };
-
-// makeDancer.prototype.setPosition = function(top, left){
-//   var styleSettings = {
-//     top: top,
-//     left: left
-//   };
-//   this.$node.css(styleSettings);
-// };
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
 
